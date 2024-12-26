7c6f0dc83e57af45914754e81cf97fa3aa7beb83
Renuka-13-Bhagya <renukalolabhattu13@gmail.com>
2024-12-26 23:39:51 +0530

    Add files via upload

---
+++ Variablesdemo.java
@@ -0,0 +1,16 @@
+class Variablesdemo
+{
+	public static void main(String args[])
+	{
+		/*int a,b;
+		a=1013;
+		System.out.println(a);
+		a=1000;
+		b=1213;
+		System.out.println(b);
+		System.out.println(a);*/
+
+		int a=100;
+		System.out.println(a);
+	}
+}
